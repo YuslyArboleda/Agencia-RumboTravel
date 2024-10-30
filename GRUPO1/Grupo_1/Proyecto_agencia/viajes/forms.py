e8bb31a637f7a40613aa4c1160e5d11f7da7d2ad
Yusly Arboleda <yuslyarboleda1@gmail.com>
2024-10-30 16:30:34 -0500

    Proyecto RumboTravel

--- GRUPO1/Grupo_1/Proyecto_agencia/viajes/forms.py
+++ GRUPO1/Grupo_1/Proyecto_agencia/viajes/forms.py
@@ -4,40 +4,44 @@ from .models import PaqueteTour, Hospedaje, HospedajeAcomodacion
 
 
 class PaqueteTourForm(forms.ModelForm):
-    hospedaje = forms.ModelChoiceField( #modelo, modelchoicefield es para decirle que es un select SSS
+    hospedaje = forms.ModelChoiceField(  # modelo, modelchoicefield es para decirle que es un select SSS
         queryset=None,  # Será asignado dinámicamente en el __init__
         required=True,
         label="Hospedaje",
     )
 
-    tarifa = forms.DecimalField( # solo numeros
+    tarifa = forms.DecimalField(  # solo numeros
         widget=forms.NumberInput(attrs={"placeholder": "Tarifa"})
     )
 
-    class Meta: #para ubicarme en el modelo que necesito
+    class Meta:  # para ubicarme en el modelo que necesito
         model = PaqueteTour
-        fields = ["hospedaje","id_hospedaje_acomodacion","id_paquete",]
-    
-    def __init__(self, *args, **kwargs): #como va a arracar cuando le de click
-        super().__init__(*args, **kwargs) #metodo constructor
+        fields = [
+            "hospedaje",
+            "id_hospedaje_acomodacion",
+            "id_paquete",
+        ]
+
+    def __init__(self, *args, **kwargs):  # como va a arracar cuando le de click
+        super().__init__(*args, **kwargs)  # metodo constructor
         print("formulario")
 
         print("Instancia", self.instance)
         print("Instancia pk", self.instance.pk)
         print("Intncia", self.instance)
         # Inicializamos los querysets, se obtiene toda la informacion
-        self.fields["hospedaje"].queryset = Hospedaje.objects.all().distinct() 
+        self.fields["hospedaje"].queryset = Hospedaje.objects.all().distinct()
         self.fields["id_hospedaje_acomodacion"].queryset = (
-                HospedajeAcomodacion.objects.none()
+            HospedajeAcomodacion.objects.none()
         )
 
         # Detectamos múltiples hospedajes enviados en los datos del formulario
         hospedajes_ids = []
         for key in self.data.keys():
             clave = re.match(
-                r"^paquete-(\d+)-hospedaje$", key
+                r"^paquetetour_set-(\d+)-hospedaje$", key
             )  # captura de cualquier entrada con el formato paquete-<número>-hospedaje.
-            print(clave)
+            print("clave", clave)
             if clave:
                 hospedaje_id = self.data.get(key)
                 if hospedaje_id:
@@ -55,7 +59,7 @@ class PaqueteTourForm(forms.ModelForm):
 
         # Si es edición, inicializamos con los valores existentes
         elif self.instance.pk:
-            
+
             hospedaje_acomodacion = self.instance.id_hospedaje_acomodacion
             print("hospedaje_acomodacion", hospedaje_acomodacion)
             self.fields["hospedaje"].initial = hospedaje_acomodacion.id_hospedaje
